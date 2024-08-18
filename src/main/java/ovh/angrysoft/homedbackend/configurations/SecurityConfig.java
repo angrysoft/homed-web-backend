1dd19c5e194c846656810d00e0c3b5bb44a05c5b
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2024-08-18 22:03:02 +0200

    remeber me ...

--- src/main/java/ovh/angrysoft/homedbackend/configurations/SecurityConfig.java
+++ src/main/java/ovh/angrysoft/homedbackend/configurations/SecurityConfig.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -43,10 +42,13 @@ public class SecurityConfig {
                 .exceptionHandling(exceptionHandling -> exceptionHandling
                         .authenticationEntryPoint(new Http403ForbiddenEntryPoint()))
                 .oauth2Login(oath2 -> {
-                    // oath2.loginPage("/api/v1/login").permitAll();
                     oath2.successHandler(oAuth2LoginSuccessHandler);
                 })
-                .rememberMe(Customizer.withDefaults())
+                .rememberMe(rem -> {
+                    rem.alwaysRemember(true);
+                    rem.authenticationSuccessHandler(oAuth2LoginSuccessHandler);
+                    rem.tokenValiditySeconds(1296000);
+                })
                 .build();
     }
 
